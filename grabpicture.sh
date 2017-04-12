1b2f4d3057ede0091dad10483bffddd3ebe7f8d8
Sanyam Goel <sanyam.bvcoe96@gmail.com>
2017-04-12 21:32:23 +0530

    test commit
    This is a sample comment in shell file.

--- grabpicture.sh
+++ grabpicture.sh
@@ -5,4 +5,4 @@ avconv -f video4linux2 -s vga -i /dev/video0 -vframes 20 /tmp/incorrectAccess/vi
 #ffmpeg -f video4linux2 -s vga -i /dev/video0 -vframes 20 /tmp/incorrectAccess/vid-$ts.%01d.jpg
 #take 20 pictures
 exit 0  #important - has to exit with status 0
-
+#comment
